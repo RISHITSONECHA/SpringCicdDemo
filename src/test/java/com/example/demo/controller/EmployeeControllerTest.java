eb1e971426d6fd836403bd5989c4aa9cf8055642
RISHITSONECHA <rishit.sonecha@gmail.com>
2024-08-10 17:51:56 +0530

    initial commit

---
+++ src/test/java/com/example/demo/controller/EmployeeControllerTest.java
@@ -0,0 +1,17 @@
+package com.example.demo;
+
+import org.junit.jupiter.api.Test;
+import static org.junit.jupiter.api.Assertions.assertEquals;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class EmployeeControllerTests {
+
+	@Test
+	public void hello() {
+		String hello = "hello";
+		assertEquals("hello", hello);
+
+	}
+
+}
